7eda9109d06e5e4070e756435fba6c51afcd5fd6
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-12 10:22:26 +0800

    Implement logic to get contacts and show result to ListView

--- app/src/main/java/com/suhun/phonebookaccess/MainActivity.java
+++ app/src/main/java/com/suhun/phonebookaccess/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -78,6 +81,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void getPhoneBookFun(View view){
-
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        while(cursor.moveToNext()){
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String number = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            String outData = String.format("%s:%s", name, number);
+            HashMap<String, String> content = new HashMap<>();
+            content.put(from[0], outData);
+            data.add(content);
+            simpleAdapter.notifyDataSetChanged();
+        }
+        cursor.close();
     }
 }
